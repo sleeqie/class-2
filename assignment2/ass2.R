ec17c19a0568b36040bc4470196ba41ab5e50ae2
Adedamola Yusuf <adedamolayusuf@Adedamolas-MacBook-Pro.local>
2020-10-18 20:45:48 +0200

    updates ass2

---
+++ assignment2/ass2.R
@@ -0,0 +1,16 @@
+#I used iris to make different types of graphs in R
+#Iris and sepal lenght to visualize histogram
+hist(iris$Sepal.Length, breaks = 10, col = "orange", 
+     main = "Histogram of Tree heights", 
+     xlab = "Height Bin")
+
+#I used trees here to visualize scatterplots
+attach(trees)
+plot(Girth, Height, main = "Scatterplot of Girth vs Height", xlab = "Tree Girth", ylab = "Tree Height")
+abline(lm(Height ~ Girth), col = "blue", lwd = 2)
+
+#Trees for Boxplot datasets
+boxplot(trees, col = c("pink", "red", "cyan"), main = "Boxplot for trees dataset")
+
+
+
